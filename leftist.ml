0b651c7e785c137137996cb715713e8dbb496c2b
mmmako <44617605+mmmako@users.noreply.github.com>
2018-11-07 13:00:07 +0100

    poprawki stylistyczne

--- leftist.ml
+++ leftist.ml
@@ -1,3 +1,6 @@
+(* Autor: Michal Makowski *)
+(* Code reviewer: Jakub Zarzycki *)
+
 type 'a queue = Node of 'a queue * 'a * 'a queue * int | Leaf;;
 
 exception Empty;;
@@ -5,15 +8,15 @@ exception Empty;;
 let empty = Leaf;;
 
 let rec join q1 q2 =
-  match q1, q2 with
-  | Leaf, x | x, Leaf -> x
-  | Node (l1, v1, r1, h1), Node (l2, v2, r2, h2) ->
-    if v1 > v2 then join q2 q1 else
-    let new_r = join r1 q2 in
-    if l1 = Leaf then Node (new_r, v1, Leaf, h1) else
-    let Node (_, _, _, h_l), Node (_, _, _, h_r) = l1, new_r in
-    if h_l < h_r then Node (new_r, v1, l1, h1 + 1) else
-    Node (l1, v1, new_r, h1 + 1)
+  match (q1, q2) with
+    | Leaf, x | x, Leaf -> x
+    | Node (l1, v1, r1, h1), Node (l2, v2, r2, h2) ->
+      if v1 > v2 then join q2 q1 else
+        let new_r = join r1 q2 in
+        if l1 = Leaf then Node (new_r, v1, Leaf, h1) else
+          let Node (_, _, _, h_l), Node (_, _, _, h_r) = l1, new_r in
+          if h_l < h_r then Node (new_r, v1, l1, h1 + 1) else
+            Node (l1, v1, new_r, h1 + 1);;
 
 let add e q = join q (Node (Leaf, e, Leaf, 1));;
 
